198254547ff78dd47020a219e98b227e19e473fa
MegaBraydn <Braydnl@gmail.com>
2015-08-23 18:50:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,19 @@ API.on(API.CHAT_COMMAND, callback);
 function callback(value) {
   alert(value + ' typed as chat command');
 }
-API.on(API.CHAT, function(data){
-if(data.type === "message" && data.message === "!skip"){
-API.moderateDeleteChat(data.cid);
-API.moderateForceSkip()
+
+API.on(API.CHAT, function(data) {
+if(data.type === "message" && data.message === "!skip") {
+    API.moderateDeleteChat(data.cid);
+    API.moderateForceSkip()
 }
 });
-API.on(API.CHAT, function(data){
-  if(data.type === "message" && data.message === "!testcmd"){
-    if (data.un === "zenopie"){
-      alert(API.getStaff());
-    }
+
+API.on(API.CHAT, function(data) {
+  if(data.type === "message" && data.message === "!testcmd") {
+    if (data.un === "zenopie") { 
+      //alert(API.getStaff());
+      alert("testcmd");
     }
   }
-}
+});

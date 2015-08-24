12e5ce751d43a3aaf39a17fc96db40b736c32527
MegaBraydn <Braydnl@gmail.com>
2015-08-24 02:53:33 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,10 @@ API.on(API.CHAT, function(data) {
                 if (staff[i].role > 1) {
                     var movearray = data.message.split(" ");
                     var wl = API.getWaitList();
-                    alert(JSON.stringify(wl));
+                    for (var i = 0, l = staff.length; i < l; i++) {
+                        if (movearry[2].substring(1) === wl.username[i]) {
+                            API.moderateMoveDJ(userID, position)
+                        }
                 }
             }
         }

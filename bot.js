91eab7fa495314dc3fc198f313ed7368ed94a973
MegaBraydn <Braydnl@gmail.com>
2015-08-23 18:23:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,3 +2,8 @@ API.on(API.CHAT_COMMAND, callback);
 function callback(value) {
   alert(value + ' typed as chat command');
 }
+API.on(API.CHAT, function(data){
+if(data.type === "message" && data.message === "!commands"){
+API.moderateDeleteChat(data.cid);
+}
+});

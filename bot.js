8084eeaf4c181b8d4e1eac0d777e3d4ff862f67e
MegaBraydn <Braydnl@gmail.com>
2015-08-24 03:29:12 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,9 +93,7 @@ API.on(API.CHAT, function(data) {
                     ma = data.message.split(" ");
                     var wl = [];
                     wl = API.getWaitList();
-                    alert(JSON.stringify(ma));
                     alert(JSON.stringify(wl));
-                    alert(JSON.stringify(ma[2]));
                     for (var i = 0, l = staff.length; i < l; i++) {
                         if (ma[1].substring(1) === wl[i].username) {
                             API.moderateMoveDJ(wl.id, ma[2]);

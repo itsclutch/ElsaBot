16615e3f3d0e64475059d4f3f927e9017d13fafb
MegaBraydn <Braydnl@gmail.com>
2015-08-24 17:38:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,3 +61,38 @@ API.on(API.CHAT, function(data) {
         }
     }
 });
+
+//move command
+
+API.on(API.CHAT, function(data) {
+    if (data.type === "message" && data.message.substring(0,5) === "!move") {
+        var staff = [];
+        staff = API.getStaff();
+        for (var i = 0, l = staff.length; i < l; i++) {
+            if (data.un === staff[i].username) {
+                if (staff[i].role > 1) {
+                    var ma = [];
+                    ma = data.message.split(" ");
+                    var wl = [];
+                    wl = API.getWaitList();
+                    for (var i = 0, l = staff.length; i < l; i++) {
+                        if (ma[1].substring(1) === wl[i].username) {
+                            API.moderateMoveDJ(wl[i].id, ma[2]);
+                        } else {
+                            API.moderateLockWaitList(true, false);
+                            var all = [];
+                            all = API.getUsers();
+                            for (var i = 0, l = all.length; i < l; i++) {
+                                if (ma[1].substring(1) === all[i].username) {
+                                    API.moderateAddDJ(all[i].id);
+                                    setTimeout()
+                                }
+                            }
+                        }
+                        }
+                    }    
+                }
+            }
+        }
+    }
+});

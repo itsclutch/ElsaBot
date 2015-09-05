b6640ad1a3ce5eaf98b0ecc24861989bb55e458b
MegaBraydn <Braydnl@gmail.com>
2015-09-04 22:24:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 API.on(API.CHAT, function(data) {
   if (data.un === "CarlTheAlpaca") {
-    API.sendChat("@CarlTheAlpaca /n https://embed.gyazo.com/e29dbe75821122c9a2cd0c0a1bf092df.png");
+    API.sendChat("https://embed.gyazo.com/e29dbe75821122c9a2cd0c0a1bf092df.png");
   }
 });
